f502461e59d74422ed9ac5a8a477433c33f6acb5
ruby94412 <ruby94412@gmail.com>
2022-04-30 16:34:13 +0800

    change to a web url img

--- anga-electric/src/App.js
+++ anga-electric/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 
+const testAddress = 'https://static.theprint.in/wp-content/uploads/2022/02/egfefrrrrrure20220jj22412412620220224134547.jpg?compress=true&quality=80&w=800&dpr=2.0'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="/images/1.jpeg" className="App-logo" alt="logo" />
+        <img src={testAddress} className="App-logo" alt="logo" />
       </header>
     </div>
   );
