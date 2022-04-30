e3be8d4e2c882c5f755550cb27be45d5b6c2812c
ruby94412 <ruby94412@gmail.com>
2022-04-30 15:27:36 +0800

    img size test

--- anga-electric/src/App.js
+++ anga-electric/src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="/images/2.jpg" className="App-logo" alt="logo" />
+        <img src="/images/1.jpeg" className="App-logo" alt="logo" />
       </header>
     </div>
   );
