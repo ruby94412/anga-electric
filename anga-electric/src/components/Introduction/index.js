cdd9ea8bfdd6e395f09858aca792e67f9c84fe2e
ruby94412 <ruby94412@gmail.com>
2022-05-10 14:34:36 +0800

    small platform test

--- anga-electric/src/components/Introduction/index.js
+++ anga-electric/src/components/Introduction/index.js
@@ -3,8 +3,8 @@ import './introduction.css'
 export default () => {
     return (
         <div className="background-introduction">
-                <pre style={{fontSize: '50px', lineHeight: '200px', fontWeight: '700'}}>20年专注</pre>
-                <p style={{fontSize: '25px', lineHeight: '50px'}}>建筑电气领域</p>
+            <h1 style={{fontSize: '58px', lineHeight: '58px', fontWeight: '700', color: '#124710'}}>20年专注</h1>
+            <h1 style={{fontSize: '58px', lineHeight: '58px', fontWeight: '700', color: '#124710'}}>建筑电气领域</h1>
         </div>
     );
 };
